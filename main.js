988187f22a0ae561d28169ba4efe38035e323fa4
Prakriti123456 <93470873+Prakriti123456@users.noreply.github.com>
2023-06-05 17:35:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+    clown_nose=loadImage('https://i.postimg.cc/RCWKmQSz/lip-removebg-preview.png');
 }
 
 function setup() {
@@ -13,6 +17,7 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(clown_nose,noseX,noseY,30,30);
 }
 
 function take_snapshot() {
@@ -26,7 +31,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
